bcdfeb393becc93284308f7248f4424b0f8e0944
Alexey Zavyalov <none>
2020-09-29 19:01:39 +0300

    fix test

--- src/test/java/org/titgbot/ApplicationTest.java
+++ src/test/java/org/titgbot/ApplicationTest.java
@@ -2,9 +2,14 @@ package org.titgbot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.telegram.telegrambots.starter.TelegramBotInitializer;
 
 @SpringBootTest
 public class ApplicationTest {
+
+    @MockBean
+    private TelegramBotInitializer telegramBotInitializer;
 
     @Test
     public void contextLoads() {
